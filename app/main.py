2cce5d56c46a5c00cb7b99ca40386c47d3cab9a6
Seena Khan <35062342+seenakhan@users.noreply.github.com>
2025-07-24 04:46:30 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,1 @@
-from fastapi import FastAPI
-from azure.storage.blob import BlobServiceClient
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-app = FastAPI()
-
-CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
-
-@app.get("/containers")
-def list_containers():
-    containers = blob_service_client.list_containers()
-    return {"containers": [c.name for c in containers]}
